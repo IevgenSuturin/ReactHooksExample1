2cb4cce598a241e194640e24b956011ab83fcc30
Suturin <YSuturin@kiev.luxoft.com>
2020-07-19 17:15:38 +0300

    Create react hook application

---
+++ src/pages/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => (
+<div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">Information</h1>
+    <p className="lead">The application version is <strong>1.0.0</strong></p>
+  </div>
+</div>)
